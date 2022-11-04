8124a4702a8a35f9e76eb94ae0c96d8b87c0f21b
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-11-04 17:27:13 +0200

    view and task show

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,4 +4,44 @@
     <h2>All task</h2>
     <a href="{{route('task.create')}}">Create new task</a>
     <!-- TODO: Текущие задачи -->
-@endsection
+
+        @if (count($tasks) > 0)
+        <div class="panel panel-default">
+          <div class="panel-heading">
+           Tasks
+          </div>
+
+          <div class="panel-body">
+            <table class="table table-striped task-table">
+
+              <!-- Заголовок таблицы -->
+              <thead>
+              <rt>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Action</th>
+              </rt>
+
+              </thead>
+
+              <tbody>
+                @foreach ($tasks as $task)
+                  <tr>
+                    <td>
+                        <div>{{$task -> id}}</div>
+                    </td>
+                    <td class="table-text">
+                      <div>{{ $task->name }}</div>
+                    </td>
+
+                    <td>
+                      <!-- TODO: Кнопка Удалить -->
+                    </td>
+                  </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+        </div>
+       @endif
+    @endsection

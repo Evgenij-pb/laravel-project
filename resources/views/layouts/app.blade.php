a1fdb187962a78ada371020e6e399ca701af1abf
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-11-07 14:12:31 +0200

    text edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,11 +12,11 @@
 
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
-<h1>task list site</h1>
+<h1>Task list site</h1>
 <div class="container">
     <nav class="navbar navbar-default">
         <ul>

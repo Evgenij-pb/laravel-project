db098cf7a194eb6f94f7774d66e645dfa4db05a2
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-11-14 20:42:22 +0200

    edit task value field

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -16,15 +16,11 @@
 
         <!-- Имя задачи -->
             <div class="form-group">
-                <label for="task" class="col-sm-3 control-label">Задача</label>
+                <label for="task" class="col-sm-3 control-label">Task</label>
 
                 <div class="col-sm-6">
 
-                    @if(!old('name'))
-                        <input type="text" name="name" id="task" class="form-control" value="{{ $task->name }}">
-                    @else
-                        <input type="text" name="name" id="task" class="form-control" value="{{ old('name') }}">
-                    @endif
+                        <input type="text" name="name" id="task" class="form-control" value="{{ old('name') ??  $task->name }}">
 
                 </div>
 
@@ -32,7 +28,7 @@
             <div class="form-group">
                 <div class="col-sm-offset-3 col-sm-6">
                     <button type="submit" class="btn btn-default">
-                        Сохранить
+                        Save
                     </button>
                 </div>
             </div>
